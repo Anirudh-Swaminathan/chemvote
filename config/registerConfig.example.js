16e79eb52d5d7b642a10a91e137ca58590f0b3ec
nirabhratapaswi <nirabhratapaswi@gmail.com>
2016-10-22 08:59:26 +0530

    config example modified

--- config/registerConfig.example.js
+++ config/registerConfig.example.js
@@ -1,3 +1,8 @@
 module.exports = {
-    adminPwd : 'your admin password here'
+    adminPwd : 'your admin password here',
+    mysqlPass: 'mysql password here',
+    mysqlUser: 'mysql user name here',
+    mysqlDb: 'mysql database name here',
+    mysqlHost: 'the host(like localhost :p)',
+    resultPass: 'the password for results page'
 };
